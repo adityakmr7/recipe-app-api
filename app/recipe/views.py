525168a76fe80d3050f2a873b252c051b32cd403
Aditya kumar <adityakmr9672@gmail.com>
2020-07-14 09:06:16 +0530

    tag view issue

---
+++ app/recipe/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets ,mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Tag
+from recipe import serializers
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    #! manage tag in the database
+    authentication_classes = (TokenAuthentication,)
+    permission_classses = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
